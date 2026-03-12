678b43d42cdf645a6f538f9b132c035b52f41469
형규 윤 <yhk8835@naver.com>
2026-03-12 11:46:44 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CatchButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CatchmeButton = new Button();
+            SuspendLayout();
+            // 
+            // CatchmeButton
+            // 
+            CatchmeButton.Font = new Font("맑은 고딕", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            CatchmeButton.ForeColor = Color.Gold;
+            CatchmeButton.Location = new Point(271, 143);
+            CatchmeButton.Name = "CatchmeButton";
+            CatchmeButton.Size = new Size(234, 155);
+            CatchmeButton.TabIndex = 0;
+            CatchmeButton.Text = "나를 잡아봐";
+            CatchmeButton.UseVisualStyleBackColor = true;
+            CatchmeButton.Click += CatchmeButton_Click;
+            CatchmeButton.MouseEnter += CatchmeButton_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(CatchmeButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button CatchmeButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,23 @@
+namespace CatchButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CatchmeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CatchmeButton_MouseEnter(object sender, EventArgs e)
+        {
+           
+            
+
+
+        }
+    }
+}
